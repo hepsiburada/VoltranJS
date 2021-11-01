36fd1a97d62fde644bd0c6f70d03e752a8c0948f
ahmetkuslular <ahmet.kuslular@hepsiburada.com>
2021-11-01 13:19:32 +0300

    Added Request Dispatcher Layer

---
+++ src/universal/core/route/dictionary.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const ROUTE_CONFIG_TO_COMPONENT_DICT = [
+  {
+    path: path.resolve(__dirname, '../../components/RequestDispatcher/RequestDispatcher'),
+    routePath: '/RequestDispatcher'
+  }
+];
+
+module.exports = ROUTE_CONFIG_TO_COMPONENT_DICT;
