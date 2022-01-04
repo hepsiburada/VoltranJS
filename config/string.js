c70d94f1af5d9fb07ba701a0c3bde0f2381f6f47
Cihan SELİM <cihan.selim@hepsiburada.com>
2022-01-04 13:12:28 +0300

    Squashed commit of the following:
    commit 412dba33824b059cb102d78e3627f38152457652
    Author: Cihan SELİM <chnselim@outlook.com>
    Date:   Fri Dec 17 13:05:07 2021 +0300
    
        FIX server.js to customize it in other projects
    
    commit cadadbfa3c0c868a505c1ce9d2f433fd29f2ce74
    Merge: a44c5e3 86587ff
    Author: Oguzhan <aslan@oguzhan.in>
    Date:   Tue Nov 16 12:40:01 2021 +0300
    
        Merge pull request #31 from hepsiburada/fix/rollback-welcome-page
    
        fix: welcome page reverted
    
    commit 86587ff176e5bbec470c2666619a7312c133a5ee
    Author: Oğuzhan Aslan <aslan@oguzhan.in>
    Date:   Tue Nov 16 12:39:22 2021 +0300
    
        fix: welcome page reverted
    
    commit a44c5e31d129913b40a545e513c0b060fc25275c
    Author: Oğuzhan Aslan <aslan@oguzhan.in>
    Date:   Thu Nov 11 14:25:21 2021 +0300
    
        feat: updated voltran version
    
    commit d40904d46cdba0e34d914a71e1b8117a47007c6b
    Merge: 95abbe6 dd576b4
    Author: Oguzhan <aslan@oguzhan.in>
    Date:   Thu Nov 11 13:14:53 2021 +0300
    
        Merge pull request #30 from hepsiburada/feat/remove-user-agent
    
        feat: removed user agent on initial state
    
    commit dd576b44605a0b4066db53ce37067b80577c526b
    Author: Oğuzhan Aslan <aslan@oguzhan.in>
    Date:   Tue Nov 9 18:42:23 2021 +0300
    
        feat: removed user agent on initial state
    
    commit 95abbe6e7048558cb90ed9235744dec059db8b9d
    Merge: 4b1cd93 70cf0ed
    Author: Oguzhan <aslan@oguzhan.in>
    Date:   Wed Oct 27 12:39:19 2021 +0300
    
        Merge pull request #28 from hepsiburada/feat/hiddenWelcomePageOnProduction
    
        feat:welcome page is hidden in production environment
    
    commit 70cf0ed1f87bd6cdfefad7de3d52aa2df397b72b
    Author: Oğuzhan Aslan <aslan@oguzhan.in>
    Date:   Tue Oct 26 16:49:44 2021 +0300
    
        chore: version updated
    
    commit 0d9d9651576f9793beae560d635a3c3aaf7aeae6
    Author: Oğuzhan Aslan <aslan@oguzhan.in>
    Date:   Tue Oct 26 16:48:40 2021 +0300
    
        feat:welcome page is hidden in production environment

--- config/string.js
+++ config/string.js
@@ -7,18 +7,12 @@ const voltranConfig = require('../voltran.config');
 
 const prometheusFile = voltranConfig.monitoring.prometheus;
 
-function replaceString() {
-  const data = [
-    {
-      search: '__V_COMPONENTS__',
-      replace: normalizeUrl(voltranConfig.routing.components),
-      flags: 'g'
-    },
+function replaceString () {
+	const data = [
+		{ search: '__V_COMPONENTS__', replace: normalizeUrl(voltranConfig.routing.components), flags: 'g' },
     {
       search: '__APP_CONFIG__',
-      replace: normalizeUrl(
-        `${voltranConfig.appConfigFile.output.path}/${voltranConfig.appConfigFile.output.name}.js`
-      ),
+      replace: normalizeUrl(`${voltranConfig.appConfigFile.output.path}/${voltranConfig.appConfigFile.output.name}.js`),
       flags: 'g'
     },
     {
@@ -26,31 +20,15 @@ function replaceString() {
       replace: normalizeUrl(`${voltranConfig.inputFolder}/assets.json`),
       flags: 'g'
     },
-    {
-      search: '__V_DICTIONARY__',
-      replace: normalizeUrl(voltranConfig.routing.dictionary),
-      flags: 'g'
-    },
-    {
-      search: '__V_REQUEST_CONFIGS__',
-      replace: normalizeUrl(voltranConfig.routing.requestConfigs),
-      flags: 'g'
-    },
-    {
-      search: '@voltran/core',
-      replace: normalizeUrl(path.resolve(__dirname, '../src/index')),
-      flags: 'g'
-    },
+    { search: '__V_DICTIONARY__', replace: normalizeUrl(voltranConfig.routing.dictionary), flags: 'g' },
+    { search: '@voltran/core', replace: normalizeUrl(path.resolve(__dirname, '../src/index')), flags: 'g' },
+    { search: '@voltran/server', replace: normalizeUrl(path.resolve(__dirname, '../src/server')), flags: 'g' },
     { search: '"__V_styles__"', replace: getStyles() }
   ];
 
-  data.push({
-    search: '__V_PROMETHEUS__',
-    replace: normalizeUrl(prometheusFile ? prometheusFile : '../lib/tools/prom.js'),
-    flags: 'g'
-  });
+  data.push({ search: '__V_PROMETHEUS__', replace: normalizeUrl(prometheusFile ? prometheusFile : '../lib/tools/prom.js'), flags: 'g' });
 
-  return data;
+	return data;
 }
 
 module.exports = replaceString;
