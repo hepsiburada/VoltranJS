63b7771255263b23e11a33ce363d87e02ad5f920
Emre <emre.stress@gmail.com>
2022-02-23 22:51:05 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import os from 'os';
 
 // 2 thread per core. 8 core = 8 * 2 = 16 thread. 
-// This poll will be used by queue system (epoll, kqueue and iocp)
+// This pool will be used by queue system (epoll, kqueue and iocp)
 // we need to check under load.
 // cluster?
 process.env.UV_THREADPOOL_SIZE = OS.cpus().length
